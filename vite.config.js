19244863ca3f95b8feb0ffc60b29b06aa3c59205
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-03-23 03:54:48 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": "https://swole-society-api-demo.vercel.app/",
-      "/uploads/": "https://swole-society-api-demo.vercel.app/",
-    },
-  },
 })
