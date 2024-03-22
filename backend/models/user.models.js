b35f437490ad564ca75c820e730ea574516789dc
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-03-23 03:03:43 +0500

    first commit

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
   {
@@ -37,12 +37,12 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcryptjs.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcryptjs.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 userSchema.methods.generateAccessToken = function () {
