a42da80dd46bb2fdd1b3d85b3989036c2cffda55
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-03-31 09:58:11 +0500

    responsive home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,21 +20,21 @@ const Home = () => {
         </Message>
       ) : (
         <>
-          <div className="flex justify-between items-center">
-            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
+          <div className="flex flex-wrap justify-center items-center md:flex-nowrap md:justify-around">
+            <h1 className="w-[80vw] ml-[25vw] mt-[5rem] text-[8vw] md:text-[4vw] md:w-[30vw] md:ml-[8vw] lg:ml-[4vw]">
               Special Products
             </h1>
 
             <Link
               to="/shop"
-              className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem] text-white"
+              className=" bg-pink-600 font-bold rounded-full py-2 px-[10vw] mt-[2rem] ml-[5vw] md:px-10 md:mr-[0vw] md:ml-[0rem] md:mt-[5rem] lg:ml-[15vw] text-white hover:bg-pink-900"
             >
               Shop
             </Link>
           </div>
 
           <div>
-            <div className="flex justify-center flex-wrap mt-[2rem]">
+            <div className="flex justify-center flex-wrap mt-[2rem] w-[78vw] ml-[18vw] md:ml-[16vw] lg:w-[90vw] lg:ml-[8vw]">
               {data.data.products.map((product) => (
                 <div key={product._id}>
                   <Product product={product} />
