9cf82d9ce541aaafb89ed006beb768f53d23fd0c
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-03-31 10:01:40 +0500

    remove product carousel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,18 +16,9 @@ const Header = () => {
 
   return (
     <>
-      <div className="flex justify-around">
-        <div className="xl:block lg:hidden md:hidden:sm:hidden">
-          <div className="grid grid-cols-2">
-            {data.data.map((product) => (
-              <div key={product._id}>
-                <SmallProduct product={product} />
-              </div>
-            ))}
-          </div>
-        </div>
+      {/* <div className="flex justify-around">
         <ProductCarousel />
-      </div>
+      </div> */}
     </>
   );
 };
