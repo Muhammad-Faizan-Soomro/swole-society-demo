86660b225903228df8e6779413c4c30afe57a438
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-04-01 04:23:13 +0500

    update home for mobile screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,21 +20,21 @@ const Home = () => {
         </Message>
       ) : (
         <>
-          <div className="flex flex-wrap justify-center items-center md:flex-nowrap md:justify-around">
-            <h1 className="w-[80vw] ml-[25vw] mt-[5rem] text-[8vw] md:text-[4vw] md:w-[30vw] md:ml-[8vw] lg:ml-[4vw]">
+          <div className="flex flex-wrap justify-center items-center md:justify-around md:mx-5 lg:mx-14">
+            <h1 className="w-[80vw] text-center mt-[5rem] text-[8vw] md:text-[4vw] md:w-[30vw] lg:ml-[4vw] xl:text-[3rem]">
               Special Products
             </h1>
 
             <Link
               to="/shop"
-              className=" bg-pink-600 font-bold rounded-full py-2 px-[10vw] mt-[2rem] ml-[5vw] md:px-10 md:mr-[0vw] md:ml-[0rem] md:mt-[5rem] lg:ml-[15vw] text-white hover:bg-pink-900"
+              className=" bg-pink-600 font-bold rounded-full py-2 px-[10vw] mt-[2rem] md:px-10 md:mr-[0vw] md:ml-[0rem] md:mt-[5rem] lg:ml-[15vw] text-white hover:bg-pink-900"
             >
               Shop
             </Link>
           </div>
 
           <div>
-            <div className="flex justify-center flex-wrap mt-[2rem] w-[78vw] ml-[18vw] md:ml-[16vw] lg:w-[90vw] lg:ml-[8vw]">
+            <div className="flex justify-center flex-wrap mt-[2rem] w-[100%] md:grid md:grid-cols-2 lg:w-[90vw] lg:flex lg:ml-[8vw]">
               {data.data.products.map((product) => (
                 <div key={product._id}>
                   <Product product={product} />
