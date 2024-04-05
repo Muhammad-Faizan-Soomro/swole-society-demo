85ed169d757e850d4b50465e94e22cd9f3d21e92
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-04-06 03:55:59 +0500

    update interface for mobile screens

--- src/components/CategoryForm.jsx
+++ src/components/CategoryForm.jsx
@@ -10,7 +10,7 @@ const CategoryForm = ({
       <form onSubmit={handleSubmit} className="space-y-3">
         <input
           type="text"
-          className="py-3 px-4 border rounded-lg w-full"
+          className="py-3 px-4 border-2 border-black rounded-lg w-full"
           placeholder="Enter category name"
           value={value}
           onChange={(e) => setValue(e.target.value)}
